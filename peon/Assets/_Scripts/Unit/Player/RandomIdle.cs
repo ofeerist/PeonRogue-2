eb46da7dcbf8038df0f9a81e7e01cc2f84481705
ofeerist <artemgorbachev046@gmail.com>
2021-07-19 12:58:12 +0600

    Random Idle Animation

---
+++ peon/Assets/_Scripts/Unit/Player/RandomIdle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game.Unit
+{
+    class RandomIdle : MonoBehaviour
+    {
+        private Animator _animator;
+
+        [SerializeField] private int min;
+        [SerializeField] private int max;
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            _animator.SetInteger("RandomIdle", Random.Range(min, max));
+        }
+    }
+}
