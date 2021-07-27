906f595a011bee4648aedf3a1aeb28f514754233
ofeerist <artemgorbachev046@gmail.com>
2021-07-27 18:48:47 +0600

    Animator optimization

---
+++ peon/Assets/_Scripts/Unit/AnimatorDisabler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Game.Unit
+{
+    class AnimatorDisabler : MonoBehaviour
+    {
+        private Animator _animator;
+
+        private void Start()
+        {
+            _animator = GetComponentInParent<Animator>();
+        }
+        private void OnBecameInvisible()
+        {
+            _animator.enabled = false;
+        }
+
+        private void OnBecameVisible()
+        {
+            _animator.enabled = true;
+        }
+    }
+}
