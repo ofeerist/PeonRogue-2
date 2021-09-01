fa1b82e20d7c67a9bb9737f223654ce749973791
ofeerist <artemgorbachev046@gmail.com>
2021-09-01 21:36:11 +0600

    Fixed urn animation

--- peon/Assets/_Scripts/Unit/AnimatorDisabler.cs
+++ peon/Assets/_Scripts/Unit/AnimatorDisabler.cs
@@ -1,11 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UniRx;
+using UnityEngine;
 
 namespace _Scripts.Unit
 {
     class AnimatorDisabler : MonoBehaviour
     {
         private Animator _animator;
-
+        [SerializeField] private GameObject _toDestroy;
+        [SerializeField] private ParticleSystem _destroyEffect;
+        
         private void Start()
         {
             _animator = GetComponentInParent<Animator>();
@@ -18,6 +22,22 @@ namespace _Scripts.Unit
         private void OnBecameVisible()
         {
             _animator.enabled = true;
+        }
+
+        public void Disable()
+        {
+            _animator.enabled = false;
+            
+            Observable.Timer(TimeSpan.FromSeconds(_destroyEffect.main.duration)).Subscribe(x =>
+            {
+                Destroy(_toDestroy);
+            }).AddTo(this);
+        }
+
+        public void Effect()
+        {
+            var eff = Instantiate(_destroyEffect, _toDestroy.transform);
+            eff.Play();
         }
     }
 }
